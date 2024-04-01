ba9d263be1975576bb48c26405e0f78cbae81d41
grapsgoober <twinklierre@gmail.com>
2024-04-01 18:21:41 +0700

    optimized is_probably_prime with simple small factor check

--- RSA/generate_keys.py
+++ RSA/generate_keys.py
@@ -1,7 +1,12 @@
 import random
 
-# Miller-Rabin primality test using 20 iterations
-def is_probably_prime(n, iter = 20) -> bool:
+# Miller-Rabin primality test using 15 iterations
+def is_probably_prime(n, iter = 15) -> bool:
+	# simple small factor primality check first, to catch easy cases
+	for i in [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]:
+		if n % i == 0:
+			return False
+	# run the Miller-Rabin algorithm
 	randnum = random.randint(2, n - 2)
 	x = pow(randnum, (n - 1) >> 1, n)
 	if x == 1 or x == n - 1:

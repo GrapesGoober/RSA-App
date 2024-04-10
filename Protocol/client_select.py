e1567c856a08d5df6f10561ab2e09b32eb5ca8d0
Graps <twinklierre@gmail.com>
2024-04-10 20:22:08 +0700

    implemented client chatroom

--- Protocol/client_select.py
+++ Protocol/client_select.py
@@ -1,17 +1,47 @@
-import socket, select                                                                                                                                
+import socket, select, threading                                                                                                                                
 
-host = '127.0.0.1'
-port = 5000
+messages = [ ] # outgoing message queue
+is_running = False
+sock: socket.socket = None
+select_inout = [ ]
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((host, port))
-inout = [socket]
+def connect_chatroom(ip: str, port: int):
+    global sock, is_running
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+    select_inout.append(sock)
+    is_running = True
+    chatroom = threading.Thread(target=chatroom_thread)
+    chatroom.start()
+
+def chatroom_thread():
+    while is_running:
+        readable, writable, _ = select.select(select_inout, select_inout, [])
+        for s in readable:
+            data = s.recv(1024)
+            if data:
+                print(data)
+            if not data:
+                print("THIS CHATROOM IS TERMINATED")
+                sock.close()
+                return
+        for s in writable:
+            if messages:
+                s.sendall(messages[0]) 
+                messages.pop(0)
+    sock.close()
+
+connect_chatroom("127.0.0.1", 5000)
 
 while True:
-    infds, outfds, errfds = select.select(inout, inout, [], 5)
-    if infds:
-        buf = socket.recv(1024)
-        if len(buf) != 0:
-            print('receive data:', buf)
-    if outfds:
-        socket.sendall(b"python select client from Debian.\n")
+    m = input()
+
+    if m == "END": 
+        is_running = False
+        break
+
+    # if m == "LOG":
+    #     for i in s.message_log: print(i)
+    #     continue
+
+    messages.append(m.encode())

d7365468cbda93d60fc6505a1ece07a5750506e1
Graps <twinklierre@gmail.com>
2024-04-10 20:05:23 +0700

    implemented server chatroom class

--- Protocol/server_select.py
+++ Protocol/server_select.py
@@ -1,41 +1,42 @@
 import select, socket, threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setblocking(False)
-server_address = ('127.0.0.1', 5000)
-server.bind(server_address)
-server.listen()
-inputs = [ server ]
-messages = [ ] # outgoing message queue
-conn = None
-is_running = True
-def chatroom_loop():
-    while is_running:
-        readable, writable, _ = select.select(inputs, inputs, [])
+class ServerChatroom:
+    def __init__(self, port: int) -> None:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setblocking(False)
+        self.sock.bind(("127.0.0.1", port))
+        self.sock.listen()
+        self.inputs = [ self.sock ]
+        self.messages = [ ] # outgoing message queue
+        self.conn = None
+        self.is_running = True
+        chatroom_thread = threading.Thread(target=self.__chatroom_thread)
+        chatroom_thread.start()
 
-        for s in readable:
-            if s is server:
-                conn, addr = s.accept()
-                print(f"connected from {addr}")
-                inputs.append(conn)
-            else: 
-                data = s.recv(1024)
-                if data:
-                    print(data)
-                if not data:
-                    print("THIS CHATROOM IS TERMINATED")
-                    server.close()
-                    return
-        for fds in writable:
-            if messages:
-                fds.sendall(messages[0]) 
-                messages.pop(0)
-    
-    server.close()
+    def __chatroom_thread(self):
+        while self.is_running:
+            readable, writable, _ = select.select(self.inputs, self.inputs, [])
+            for s in readable:
+                if s is self.sock:
+                    conn, addr = s.accept()
+                    print(f"connected from {addr}")
+                    self.inputs.append(conn)
+                else: 
+                    data = s.recv(1024)
+                    if data:
+                        print(data)
+                    if not data:
+                        print("THIS CHATROOM IS TERMINATED")
+                        self.sock.close()
+                        return
+            for s in writable:
+                if self.messages:
+                    s.sendall(self.messages[0]) 
+                    self.messages.pop(0)
+        self.server.close()
 
-chatroom_thread = threading.Thread(target=chatroom_loop)
-chatroom_thread.start()
 
+chat = ServerChatroom(5000)
 
 while True:
     m = input()
@@ -48,5 +49,5 @@ while True:
     #     for i in s.message_log: print(i)
     #     continue
 
-    messages.append(m.encode())
+    chat.messages.append(m.encode())
 

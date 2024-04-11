f9671438d3c534725cd5ca70320f5c7ef1fe05b6
Graps <twinklierre@gmail.com>
2024-04-11 16:28:59 +0700

    reorganized server connection accept function

--- Protocol/chatroom.py
+++ Protocol/chatroom.py
@@ -11,27 +11,32 @@ class Chatroom:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.is_running = True
         match mode:
-            case "server": self.__start_server(ip, port)
-            case "client": self.__connect_server(ip, port)
+            case "server": self.start_server(ip, port)
+            case "client": self.connect_server(ip, port)
             case _: 
                 raise Exception("Chatroom mode supports only 'server' and 'client'")
         
-        chatroom = threading.Thread(target=self.__chatroom_message_thread)
+    def start_server(self, ip: str, port: int) -> None:
+        self.sock.bind((ip, port))
+        self.sock.listen()
+        connect_thread = threading.Thread(target=self.await_conn_then_chat)
+        connect_thread.start()
+
+    def connect_server(self, ip: str, port: int) -> None:
+        self.sock.connect((ip, port))
+        self.inout.append(self.sock)
+        chatroom = threading.Thread(target=self.handle_chat)
         chatroom.start()
-    
-    def __start_server(self, ip: str, port: int) -> None:
-            self.sock.bind((ip, port))
-            self.sock.listen()
-            # the server only accepts 1 connection, and is a blocking code
-            conn, addr = self.sock.accept()
-            print(f"connected from {addr}")
-            self.inout.append(conn)
 
-    def __connect_server(self, ip: str, port: int) -> None:
-            self.sock.connect((ip, port))
-            self.inout.append(self.sock)
+    def await_conn_then_chat(self):
+        # receive connection first
+        conn, addr = self.sock.accept()
+        print(f"connected from {addr}")
+        self.inout.append(conn)
+        # once the connection is established, start chatroom
+        self.handle_chat()
 
-    def __chatroom_message_thread(self) -> None:
+    def handle_chat(self) -> None:
         while self.is_running:
             readable, writable, _ = select.select(self.inout, self.inout, [])
             for s in readable:

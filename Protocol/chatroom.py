4ae21db9feae55c358240a14b1f8d2aaa72d5036
grapsgoober <twinklierre@gmail.com>
2024-04-11 23:24:17 +0700

    written connection reset handler

--- Protocol/chatroom.py
+++ Protocol/chatroom.py
@@ -1,27 +1,33 @@
-from Protocol.keys import Apriv, Apub, Bpriv, Bpub
 import select, socket
 
 messages:   list[bytes] = []
 server =    socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sockets:    list[socket.socket] = [server]
+users:      dict[socket.socket, dict] = {}
 
+# Persistent TCP
 def start_chatroom(ip: str, port: int):
     server.setblocking(False)
     server.bind((ip, port))
-    server.listen()
+    server.listen(2)
     while True:
         readable: list[socket.socket]
         readable, _, _ = select.select(sockets, [], [])
         for s in readable:
-            if s is server: # server receives a new connection
+            if s is server:     # server receives a new user connection
                 conn, addr = s.accept()
                 print(f"connected from {addr}")
                 sockets.append(conn)
-            else:           # connection receives new data
-                data = s.recv(1024)
+                users[conn] = {"addr": addr}
+            elif conn := s:     # connection receives new data
+                data = None
+                try: data = conn.recv(1024)
+                except ConnectionResetError: pass
                 if data: print(data)
-                if not data:
-                    print(f"user disconnects")
-                    s.close()
-                    sockets.remove(s)
+                else: handle_disconnected_user(conn)
+
+def handle_disconnected_user(conn: socket.socket):
+    print(f"user {users[conn] } disconnects")
+    conn.close()
+    sockets.remove(conn)
 

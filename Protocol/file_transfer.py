19d4bfdc481a996064e99d59d5a65683275c6e5c
grapsgoober <twinklierre@gmail.com>
2024-04-13 22:28:17 +0700

    implemented secure file transfer

--- Protocol/file_transfer.py
+++ Protocol/file_transfer.py
@@ -2,20 +2,23 @@ import socket, RSA
 from typing import Generator, Iterable
 
 # sets up a TCP server, exchange key modulus, and receives data
-def receive_stream(ip: str, port: int, key: int) -> Generator[int, None, None]:
+def receive_stream(ip: str, port: int, keys: tuple[int, int]) -> Generator[bytes, None, None]:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((ip, port))
     sock.listen()
     conn, addr = sock.accept()
     print(f"connected from {addr}")
-    conn.sendall(key.to_bytes(key.bit_length() // 8 + 1))
-    print(f"key sent")
+    k_priv, modulus = keys
+    conn.sendall(modulus.to_bytes(modulus.bit_length() // 8 + 1))
+    while data := conn.recv(1024):
+        yield RSA.decrypt(data, (k_priv, modulus))
 
 # connects to TCP server, exchange key modulus, and sends data
-def send_stream(ip: str, port: int, data: Iterable[bytes]):
+def send_data(ip: str, port: int, data: bytes):
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.connect((ip, port))
-    print(f"connected to {(ip, port)}")
     key_n = int.from_bytes(conn.recv(1024))
-    print(f"key received")
-    print(key_n)
+    encrypted = RSA.encrypt(data, (65537, key_n))
+    conn.sendall(encrypted)
+    conn.close()
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Protocol import chat_tcp, send_stream, receive_stream
+from Protocol import chat_tcp, send_data, receive_stream
 import RSA
 
 IP = "127.0.0.1"
@@ -8,7 +8,10 @@ mode = input("enter mode - receive (r), send (s)")
 match mode:
     case 'r':
         k_pub, k_priv = RSA.generate_keys(512)
-        receive_stream(IP, PORT, k_pub[1])
+        data_stream = receive_stream(IP, PORT, k_priv) # we only need d and n
+        for i in data_stream:
+            print(i)
     case 's':
-        send_stream(IP, PORT, None)
+        message = b"some random send stuff"
+        send_data(IP, PORT, message)
     case _: exit()

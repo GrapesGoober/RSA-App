796670b69ec5601eaf37a147146cb127602bbaf3
Noravorn2546 <noravorn.ac@gmail.com>
2024-04-14 14:04:07 +0700

    make new UI for file tranfer and delete unnecessary code in new branch

--- App/json_read.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open("App\config.json", "r") as f:
-    config = json.loads(f.read())
-    print(config["n"])
--- App/new_UI.py
+++ App/new_UI.py
@@ -1,31 +1,29 @@
 import json
 
-from Protocol import Chatroom
+from Protocol import send_data, receive_stream
 from RSA import generate_keys
 
-def secure_TCP_chatroom():
+def secure_file_tranfer():
     while True:
         print(""" 
-Welcome to secure TCP chatroom.
-    1 - Change Keys
-    2 - Create New Chatroom
-    3 - Connect Chatroom
-    4 - Exit
+Welcome to secure file tranfering program.
+    1 - Sending File
+    2 - Reading a recieve file
+    3 - Exit
 Press Number You Wanna Do: """)
 
         match input():
-            case "1": change_keys()
-            case "2": create_chatroom()
-            case "3": connect_chatroom()
-            case "4": exit(0)
+            case "1": sending()
+            case "2": recieving()
+            case "3": exit(0)
             case _:   print("invalid inputs")
 
-def change_keys():
+def generate_keys():
     print("The minimum key size is 256")
     size = int(input("Enter keys size: "))
     if size < 256:
         print("Too small! Try again")
-        change_keys()
+        generate_keys()
     else:
         pub, priv = generate_keys(size)
         with open("App\config.json", "r") as f:
@@ -38,12 +36,15 @@ def change_keys():
         with open("App\config.json", "w") as f:
             json.dump(config, f, indent=4)  # Write back to file with indentation
 
-def create_chatroom():
-    my_IP = input("Enter IP: ")
-    my_PORT = int(input("Enter Port: "))
-    chatroom = Chatroom(mode='server', ip=my_IP, port=my_PORT)
-
-def connect_chatroom():
+def sending():
+    generate_keys()
     dest_IP = input("Enter IP: ")
     dest_PORT = int(input("Enter Port: "))
-    chatroom = Chatroom(mode='client', ip=dest_IP, port=dest_PORT)
+    data = ""
+    send_data(dest_IP,dest_PORT,data)
+
+def recieving():
+    dest_IP = input("Enter IP: ")
+    dest_PORT = int(input("Enter Port: "))
+    key = []
+    receive_stream(dest_IP,dest_PORT,key)

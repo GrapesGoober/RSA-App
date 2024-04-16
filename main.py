27de10d6e0e60bd4daf04f72a9e8821e461f2780
Graps <twinklierre@gmail.com>
2024-04-16 16:30:38 +0700

    rewritten test code for arbitrary binary data encryption

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from RSA import generate_keys, encrypt, decrypt
+import random
 
-message = """
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
-    culpa qui officia deserunt mollit anim id est laborum.\
-""".encode()
+def generate_large_message(size: int = 300) -> bytes:
+    message_list = []
+    for _ in range(size):
+        randint = random.randrange(0, 2 ** 8)
+        randbytes = randint.to_bytes(length=1)
+        message_list.append(randbytes)
+    return b"".join(message_list)
 
+message = generate_large_message()
 pub, priv = generate_keys(512)
 cipher_text = encrypt(message, pub)
-print(cipher_text)
-original_message = decrypt(cipher_text, priv)
-print(original_message)
+decrypted = decrypt(cipher_text, priv)
+print(decrypted == message)

6921d40922b7ae8f0b0672014199f678b6ef6c44
Graps <twinklierre@gmail.com>
2024-04-16 15:58:00 +0700

    written a file generator

---
+++ Test Files/generate_large_testfile.py
@@ -0,0 +1,9 @@
+# This code is to generate a large test file full of random bytes
+import random
+
+iterations = 100_000
+size = 1024
+with open("Test Files\\random_bytes.bin", "wb") as f:
+    for _ in range(iterations):
+        randnum = random.randint(0, 2 ** (size * 8))
+        f.write(randnum.to_bytes(size))

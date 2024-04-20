1bf537eece1b94845fe3c186cab42cd73b1a808a
Noravorn2546 <noravorn.ac@gmail.com>
2024-04-20 14:13:34 +0700

    test code in main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-from Protocol import file_transfer
-import RSA
+from App import secure_file_tranfer
 
-IP = "127.0.0.1"
-PORT = 5000
-
-mode = input("enter mode - receive (r), send (s)")
-match mode:
-    case 'r':
-        print(f"generating keys")
-        keys = RSA.generate_keys(1024)
-        print(f"setting server")
-        conn, addr = file_transfer.await_conn(IP, PORT, keys)
-        print(f"connected from {addr}")
-        print(f"decrypting...")
-        data = file_transfer.receive_and_decrypt(conn, keys)
-        print(f"writing to file")
-        with open("Test Files\\random_bytes_receive.bin", "wb") as f:
-            f.write(data)
-        print(f"done")
-
-    case 's':
-        with open("Test Files\\random_bytes.bin", "rb") as f:
-            file_transfer.send_data(IP, PORT, f.read())
-    case _: exit()
+secure_file_tranfer()

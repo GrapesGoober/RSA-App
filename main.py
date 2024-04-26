1203a466e793ef052431718e6675bbc2c3cc4b7d
grapsgoober <twinklierre@gmail.com>
2024-04-26 16:11:52 +0700

    written file-transfer test code in main.py

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ PORT = 5000
 
 mode = input("enter mode - receive (r), send (s)")
 match mode:
+    # # this code here is a chat channel
+    # case 'r':
+    #     print(f"generating keys")
+    #     keys = RSA.generate_keys(512)
+    #     print(f"server awaiting connection")
+    #     with Receiver(IP, PORT, keys) as r:
+    #         print(f"connected")
+    #         while m := r.get_message(): print(m.decode())
+    # case 's':
+    #     with Sender(IP, PORT) as r:
+    #         while m := input("> "): r.send(m.encode())
+
+    
+    # this code here is a file sending channel
     case 'r':
         print(f"generating keys")
         keys = RSA.generate_keys(512)
         print(f"server awaiting connection")
         with Receiver(IP, PORT, keys) as r:
             print(f"connected")
-            while m := r.get_message(): print(m.decode())
-
+            with open("Test Files\\random_bytes_receive.bin", "wb") as f:
+                while m := r.get_message(): f.write(m)
+        print(f"done")
     case 's':
         with Sender(IP, PORT) as r:
-            while m := input("> "): r.send(m.encode())
+            with open("Test Files\\random_bytes.bin", "rb") as f:
+                while m := f.read(1024): r.send(m)
     case _: exit()
